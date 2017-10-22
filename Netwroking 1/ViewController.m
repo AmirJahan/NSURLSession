ea5e3651d8b80ea26684fbb69fa69122efd7dd1b
Amir J <amir.jahan@gmail.com>
2017-10-22 09:29:10 -0700

    Initial Commit

---
+++ Netwroking 1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Netwroking 1
+//
+//  Created by Amir on 10/22/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Netwroking 1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Netwroking 1
+//
+//  Created by Amir on 10/22/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
